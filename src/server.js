fdde33bc4dfab123e7e5dcce8ea8da9d8e211c09
zeven <frostdi40@gmail.com>
2026-09-03 21:41:59 +0300

    update

---
+++ src/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({
+    message: `Retrieved note with ID: ${req.params.noteId}`,
+  });
+});
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+app.listen(PORT, () => {
+  console.log('Server is successful running!');
+});
