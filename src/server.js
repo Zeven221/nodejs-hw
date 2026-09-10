ef5201361948572487d443508c602c258270fa7e
Saveliy <frostdi40@gmail.com>
2026-09-10 18:33:39 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import express from 'express';
-const app = express();
-const PORT = process.env.PORT ?? 3000;
 import 'dotenv/config';
 import cors from 'cors';
 import { logger } from './middleware/logger.js';
@@ -8,13 +6,15 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
- await connectMongoDB;
+ await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is successful running! Server port is ${PORT}.`);
 });
