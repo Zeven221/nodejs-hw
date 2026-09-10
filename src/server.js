19ab0e111996b4e66f20f7b842ef73db4947a246
Saveliy <frostdi40@gmail.com>
2026-09-10 18:15:15 +0300

    update

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRouter.js';
+import notesRoutes from './routes/notesRoutes.js';
 app.use(express.json());
 app.use(cors());
 app.use(logger);
